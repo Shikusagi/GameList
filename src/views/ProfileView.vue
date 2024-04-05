2d2e40c2366bcbaab127794b996657018dead3da
chieu <chieu@eurecom.fr>
2024-04-05 11:53:17 +0200

    modified profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+
+const username = 'Shiku';
+const entries = ref([
+  { id: 1, title: 'Fortnite' },
+  { id: 2, title: 'Dofus' },
+  { id: 3, title: 'Genshin Impact' },
+]);
+
+
 </script>
 
 <template>
-  Profile
+  <div>
+    <img src="https://i.imgur.com/FfqCvuB.jpeg" alt="Profile Picture" />
+    <span>{{ username }}</span>
+    <ul>
+      <li v-for="entry in entries" :key="entry.id">{{ entry.title }}</li>
+    </ul>
+  </div>
 </template>
 
+
 <style scoped>
+img {
+  width: 100px;
+  height: 100px;
+}
 </style>
