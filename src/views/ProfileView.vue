8a43a2f6810602b884a50b76c603cacb29e50fe5
chieu <chieu@eurecom.fr>
2024-04-12 14:44:55 +0200

    changed css

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,20 +1,32 @@
 <template>
-  <div>
-    <div class="band"></div>
+  <div v-if="user">
     <div>
-      <img :src="user.profilePicture" alt="Profile Picture" />
-      <span>{{ user.username }}</span>
-      <ul>
-        <li v-for="game in user.games" :key="game.id">
-          {{ game.title }} - Scored: {{ game.grade }} - Status: {{ game.status }}
-        </li>
-      </ul>
+      <div>
+        <div class="user-info">
+          <img class="user-pic" :src="user.profilePicture" alt="Profile Picture" />
+          <span class="user-name">{{ user.username }}</span>
+        </div>
+        
+        <ul>
+          <li v-for="game in user.games" :key="game.id">
+            <div class="game-item">
+              <img :src="game.pic" alt="Game Picture" class="game-picture" />
+              <span>{{ game.status }} {{ game.title }} - Scored: {{ game.grade }}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
+  <div v-else>
+    <p>You are not logged in...</p>
+   
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import users from '../data/users.json';
 
 const user = ref({
   username: '',
@@ -25,9 +37,7 @@ const user = ref({
 // Load data from JSON file
 onMounted(async () => {
   try {
-    const response = await fetch('../user_data.json');
-    const data = await response.json();
-    user.value = data.user;
+    user.value = users[0];
   } catch (error) {
     console.error('Error loading data:', error);
   }
@@ -36,44 +46,46 @@ onMounted(async () => {
 
 <style scoped>
 
-
-.band {
+.user-info{
+  display: flex;
+  flex-direction: row;
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   background-color: rgb(22, 22, 47);
   height: 300px; 
+  gap: 10px;
 }
 
-img {
-  position: absolute;
-  top: 150px; 
-  left: 15%; 
+.user-pic {
   width: 150px;
   height: 150px;
+  margin-left: 20%;
+  margin-top: 150px;
 }
 
-span {
-  position: absolute;
-  top:260px;
-  left: 27%; 
+.user-name {
   font-size: 24px;
   font-family: Snell Roundhand, cursive;
-  color: white; 
+  color: white;
+  margin-top: 260px;
+  margin-left: 15px;
 }
 
 li {
-  background-color: gray;
+  background-color: rgb(176, 191, 235);
   padding: 18px;
   margin-bottom: 20px;
   text-align: left;
   font-size: 16px;
   font-family: Arial;
   width: 50%;
-  margin-left: 40%; 
+  height: 60px;
+  margin-left: 45%; 
   margin-right: auto;
 }
+
 ul {
   display: flex;
   flex-direction: column;
@@ -82,4 +94,18 @@ ul {
   padding: 0;
   margin-top: 350px;
 }
+
+.game-picture {
+  width: 70px;
+  height: 70px;
+  margin-top: -0.5%;
+}
+
+.game-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+}
+
 </style>
